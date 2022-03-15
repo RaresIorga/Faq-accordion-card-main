87f5163d21c1b06cf401c77ec50ab54b9cc7d46b
RaresIorga <rares655@gmail.com>
2022-03-15 21:35:10 +0200

    Some Progress

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ let answers = document.querySelectorAll(".faq");
 answers.forEach((event) => {
   event.addEventListener("click", () => {
     if (event.classList.contains("active")) {
+      answers.forEach((event) => {
+        event.classList.remove("active");
+      });
       event.classList.remove("active");
     } else {
+      answers.forEach((event) => {
+        event.classList.remove("active");
+      });
       event.classList.add("active");
     }
   });
